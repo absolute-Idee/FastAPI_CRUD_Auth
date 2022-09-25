e6a6fc6e8085d23b23613b5b9d2512f7a619ae5f
maxim <urbaevmax@gmail.com>
2022-09-25 19:31:23 +0300

    tags for routes added

--- app/api.py
+++ app/api.py
@@ -19,12 +19,12 @@ def get_db():
         db.close()
 
 
-@app.get("/posts")
+@app.get("/posts", tags=["api"])
 async def get_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> dict:
     return crud.get_all_posts(db, skip=skip, limit=limit)
 
 
-@app.get("/posts/{post_id}")
+@app.get("/posts/{post_id}", tags=["api"])
 async def get_post(post_id: int, db: Session = Depends(get_db)) -> dict:
     db_post = crud.get_post(db, post_id=post_id)
     if db_post is None:
@@ -33,16 +33,16 @@ async def get_post(post_id: int, db: Session = Depends(get_db)) -> dict:
     return db_post
 
 
-@app.post("/posts", status_code=201)
+@app.post("/posts", status_code=201, tags=["api"])
 async def add_post(post: schemas.Post, db: Session = Depends(get_db)) -> dict:
     return crud.create_post(db, post=post)
 
 
-@app.put("/posts/{post_id}")
+@app.put("/posts/{post_id}", tags=["api"])
 async def update_post(post_id: int, post: PostBase, db: Session = Depends(get_db)) -> dict:
     return crud.update_or_create_post(db, post_id=post_id, post=post)
 
 
-@app.delete("/posts/delete/{post_id}", status_code=204)
+@app.delete("/posts/delete/{post_id}", status_code=204, tags=["api"])
 async def delete_post(post_id: int, db: Session = Depends(get_db)) -> dict:
     return crud.delete_by_id(db, post_id=post_id)
