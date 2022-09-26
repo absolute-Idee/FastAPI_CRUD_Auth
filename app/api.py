65db79d3add4307b4b4f1cfbc9d49a807b6e9c4f
maxim <urbaevmax@gmail.com>
2022-09-26 15:41:54 +0300

    post pu delete routes now protected

--- app/api.py
+++ app/api.py
@@ -72,15 +72,15 @@ async def get_post(post_id: int, db: Session = Depends(get_db)) -> dict:
 
 
 @app.post("/posts", status_code=201, tags=["api"])
-async def add_post(post: Post, db: Session = Depends(get_db)) -> dict:
+async def add_post(post: Post, db: Session = Depends(get_db), user: SystemUser = Depends(get_current_user)) -> dict:
     return crud.create_post(db, post=post)
 
 
 @app.put("/posts/{post_id}", tags=["api"])
-async def update_post(post_id: int, post: PostBase, db: Session = Depends(get_db)) -> dict:
+async def update_post(post_id: int, post: PostBase, db: Session = Depends(get_db), user: SystemUser = Depends(get_current_user)) -> dict:
     return crud.update_or_create_post(db, post_id=post_id, post=post)
 
 
 @app.delete("/posts/delete/{post_id}", status_code=204, tags=["api"])
-async def delete_post(post_id: int, db: Session = Depends(get_db)) -> dict:
+async def delete_post(post_id: int, db: Session = Depends(get_db), user: SystemUser = Depends(get_current_user)) -> dict:
     return crud.delete_by_id(db, post_id=post_id)
